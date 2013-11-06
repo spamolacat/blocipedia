dd75a166ba24402eafa3554758d4b2d73769f147
Matt MacDonald <spamolacat@yahoo.com>
2013-11-06 14:00:47 -0800

    basic framework

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Blocipedia::Application.routes.draw do
+  get "welcome/index"
+
+  get "welcome/about"
+
+  root to: 'welcome#index'
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
